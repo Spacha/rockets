1c9b957d7df830080ffd7ac18e3f929014c77ad9
Spacha <miikasikala96@gmail.com>
2019-09-17 17:25:40 +0300

    Basic Bootstrap page structure

--- public/index.php
+++ public/index.php
@@ -1,7 +1,7 @@
 <?php
 
 	define('ROOT_PATH', dirname(__DIR__));
-	
+
 	require_once(ROOT_PATH . '/vendor/autoload.php');
 
 ?>
@@ -25,13 +25,30 @@
 	<body>
 
 		<!-- Top Nav -->
-		<nav class="navbar navbar-dark bg-dark flex-md-nowrap p-0 shadow">
-			<a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">Rockets</a>
+		<nav class="navbar navbar-dark bg-dark flex-md-nowrap p-0 shadow justify-content-center">
+			<a class="navbar-brand col-md-2 mr-0" href="#">Rockets</a>
 		</nav>
 
-		<div class="container-fluid">
+		<div class="container pt-4">
 
-			<h1>Moikkelis hoikkelis.</h1>
+			<section>
+				<h1>1. Classification</h1>
+
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+			</section>
+
+			<section>
+				<h1>2. Definitions and Fundamentals</h1>
+
+				<p></p>
+
+			</section>
 			
 		</div>
 

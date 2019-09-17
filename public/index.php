579b0194b03b4b64ca5408d8fc1b8f6e20dcf842
Spacha <miikasikala96@gmail.com>
2019-09-17 16:17:32 +0300

    Project initialized

---
+++ public/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Rockets</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+		<link rel="stylesheet" href="<?='/css/app.css'?>">
+		<script src="/js/app.js"></script>
+	</head>
+	<body>
+		<h1>Paska kaupunni.</h1>
+	</body>
+</html>
---
+++ webpack.mix.js
@@ -0,0 +1,20 @@
+let mix = require('laravel-mix');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for your application, as well as bundling up your JS files.
+ |
+ */
+
+let sourcePath = 'app/assets';
+let publicPath = 'public';
+
+mix
+	.js(sourcePath + '/js/app.js', publicPath + '/js/')
+	.sass(sourcePath + '/sass/app.scss', publicPath + '/css/')
+	.setPublicPath(publicPath);
